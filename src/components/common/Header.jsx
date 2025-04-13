ccd3e6973a275e2e0b338bd980588c3567e4b3a3
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-04-13 23:55:28 +0100

    changed the route in the header for home

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,7 +4,7 @@ const Header =()=>{
 return (
     <header style={{position:'fixed',top:"0px", zIndex:"100", padding: '1rem', backgroundColor: '#333', color: '#fff',width:'100%' }}>
       <nav style={{ display: 'flex', gap: '1rem' }}>
-        <Link to="/Course" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+        <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
         <Link to="/categories" style={{ color: '#fff', textDecoration: 'none'}}>Categories</Link>
         
       </nav>
